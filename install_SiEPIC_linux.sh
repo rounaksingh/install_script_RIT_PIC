396e80d35ca6763692e6fee81b6d127b0efc73ae
Rounak Singh <rounaksingh17@gmail.com>
2017-02-24 03:28:13 -0500

    Klayout installation added

--- install_SiEPIC_linux.sh
+++ install_SiEPIC_linux.sh
@@ -1,24 +1,30 @@
 #! /usr/bin/env bash
 #
-# Install SiEPCI on a pre-install Klayout
+# Install Klayout & SiEPCI
 # For Linux
 # Written by Rounak Singh Narde <rn5949@rit.edu>
+# MIT License - Public is free to use, distribute and modify
 #
-# Required: Klayout plus unzip, wget is required
+# Required: unzip, wget is required. "Everything should be in home directory."
 # 
-# - Installation checks for klayout. It runs only if klayout is installed.
+# - script checks for klayout. If not installed, script installs it from the installation zip file.
 #
 # -Two way installation - offline and online
 # -- offline - installation requires already downloaded zip file. It should be in current directory. 
 # The filename should be provided below. The hierarchy should be the same as downloaded from github. 
 # -- online - script download zip file from github. A released version number should be provided below.
 #
+# Note: Script can also be used to update the SiEPIC package directly from github without changing klayout.
+# 
 #----------------------------------------------------------------------------------------------------------------------
 
-#
-# -offline- SiEPIC_file name - if you have a previously downloaded file then write a filename.
+
+# Klayout file
+KLAYOUT_INSTALL_FILE="klayout_RITLinux.zip"
+
+# -offline- SiEPIC_FILE name - if you have a previously downloaded file then write a filename.
 # Note the hierarchy of directories should be the same as downloaded from github
-SiEPIC_file="SiEPIC_EBeam_PDK-0.1.35_RITLinux.zip"
+SiEPIC_FILE="SiEPIC_EBeam_PDK-0.1.35_RITLinux.zip"
 
 # -online- Enter SiEPIC version as in github releases
 SiEPIC_VERSION="0.1.35"
@@ -29,22 +35,37 @@ SiEPIC_VERSION="0.1.35"
 config_bash_profile() 
 {
     echo "Configuring .bash_profile"
-    echo "PATH=\$PATH:\$HOME/bin:\$HOME/klayout/usr/bin" >> ~/.bash_profile
+    echo "PATH=\$PATH:\$HOME/klayout/usr/bin" >> ~/.bash_profile
     echo "export PATH" >> ~/.bash_profile
     echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$HOME/klayout/usr/lib64" >> ~/.bash_profile
     echo "export LD_LIBRARY_PATH" >> ~/.bash_profile
     echo "Configured"
 }
 
-# Check whether klayout exists
-check_klayout_exist() 
+# Install Klayout from file
+install_klayout()
 {
+    if [ -f "$KLAYOUT_INSTALL_FILE" ]
+    then
+        echo "Installing Klayout"
+        unzip -oq $KLAYOUT_INSTALL_FILE
+        echo "Installation complete"
+        config_bash_profile
+	source ~/.bash_profile
+	klayout -zz
+    else
+        echo "Klayout Installation file not found in home directory."
+        echo "Bye bye"
+        exit 3
+    fi       
+}
+
+# Check whether klayout exists
+check_klayout_exist() {
     if hash klayout 2>/dev/null; then
         echo "Klayout Found"
     else
-        echo "Klayout Not Found. Please install Klayout"
-        echo "Bye bye"
-        exit 2
+        install_klayout
     fi
 }
 
@@ -58,12 +79,11 @@ extract_zip_file()
 # Runs code for installation
 install_SiEPIC() 
 {
-
-    if [ -f "$SiEPIC_file" ]
+    if [ -f "$SiEPIC_FILE" ]
     then
         # offline
-        echo "Starting offline Installation"
-        extract_zip_file $SiEPIC_file
+        echo "Starting offline Installation - $SiEPIC_FILE"
+        extract_zip_file $SiEPIC_FILE
     else
         # online
         echo "Downloading SiEPIC_EBeam_PDK version: $SiEPIC_VERSION"
@@ -76,13 +96,19 @@ install_SiEPIC()
         extract_zip_file v$SiEPIC_VERSION.zip
         rm v$SiEPIC_VERSION.zip
     fi
-     
+    
     echo "Installing SiEPIC_EBeam_PDK version: $SiEPIC_VERSION"
     cp -r SiEPIC_EBeam_PDK-$SiEPIC_VERSION/klayout_dot_config/* ~/.klayout/
     echo "Installation complete"
 }
 
+
+echo
 check_klayout_exist
+#install_klayout
+echo
 install_SiEPIC
-config_bash_profile
+echo
+echo "Happy PIC!"
+echo
 exit 0
